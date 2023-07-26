1ed710799dabe3376393fd4c2748a903636ef2e2
CML120 <cheelor922@gmail.com>
2023-07-25 20:00:09 -0500

    added queries for viewing employees, departments, roles

--- server.js
+++ server.js
@@ -37,11 +37,114 @@ function employeeManagerPrompt() {
                     "Quit",
                 ],
             },
-        )
+        )        .then(function (choice) {
+            switch (choice.select) {
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Update Employee Role":
+                    updateEmployeeRole()
+                    break;
+                case "View All Roles":
+                    viewAllRoles();
+                    break;
+                case "Add a Role":
+                    addRole();
+                    break;
+                case "View All Departments":
+                    viewAllDepartments();
+                    break;
+                case "Add a Department":
+                    addDepartment();
+                    break;
+                default:
+                    quitProgram();
+            }
+        })
 }
 
+function viewAllEmployees() {
+    const query = `SELECT
+    emp.id AS employee_id,
+    emp.first_name,
+    emp.last_name,
+    role.title,
+    department.name AS department,
+    role.salary,
+    CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+FROM
+    employee AS emp
+LEFT JOIN
+    role ON emp.role_id = role.id
+LEFT JOIN
+    department ON role.department_id = department.id
+LEFT JOIN
+    employee AS manager ON emp.manager_id = manager.id;
 
+`;
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        employeeManagerPrompt();
+    })
+    
+};
 
+function addEmployee() {
+    
+};
+
+function updateEmployeeRole() {
+
+};
+function viewAllRoles() {
+    const query = `
+        SELECT
+            role.id AS role_id,
+            role.title,
+            role.salary,
+            department.name AS department
+        FROM
+            role
+        LEFT JOIN
+            department ON role.department_id = department.id;
+    `;
+
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        employeeManagerPrompt();
+    });
+};
+function addRole() {
+
+};
+function viewAllDepartments() {
+    const query = `
+        SELECT
+            id AS department_id,
+            name AS department
+        FROM
+            department;
+    `;
+
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        employeeManagerPrompt();
+    });
+};
+function addDepartment() {
+
+};
+
+function quitProgram() {
+    connection.end();
+    process.exit();
+  }
 
 employeeManagerPrompt();
 
